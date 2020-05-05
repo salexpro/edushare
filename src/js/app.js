1809de6d14297c5317066693113516ccf0e65e85
Alexander <dev@salex.pro>
2020-05-05 22:25:31 +0300

    lang toggler & korean version

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
+import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
 import './lib/foundation-explicit-pieces';
 import './components/_anim'
 // gsap.registerPlugin(DrawSVGPlugin);
 
 svg4everybody();
+
+$('.lang a').click(function(){
+    localStorage.setItem('lang', this.innerText.toLowerCase());
+})
